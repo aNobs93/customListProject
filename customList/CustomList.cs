1641ccc81cf07d786215b3150ed4b16144d609ca
Adam Nobs <nobsadam@gmail.com>
2019-12-05 15:31:12 -0600

    Added 3 complete tests for add method in unittest1

---
+++ customList/CustomList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace customList
+{ 
+    public class CustomList<T>
+    {
+        T[] items;
+        int capacity;
+        int count;
+
+        public CustomList()
+        {
+            count = 0;
+            capacity = 4;
+            items = new T[capacity];
+
+        }
+
+
+    }
+}
--- customListTest/UnitTest1.cs
+++ customListTest/UnitTest1.cs
@@ -7,8 +7,62 @@ namespace customListTest
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Add_IntToArray_AddToArray()
         {
+            //arrange
+            CustomList<int> customlist = new CustomList<int>();
+            int num = 5;
+            int expected = 5;
+            int actual;
+            //act
+            customlist.Add(num);
+            actual = customlist[0];
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Add_IntToArray2_AddToArray()
+        {
+            //arrange
+            CustomList<int> customlist = new CustomList<int>();
+            int num1 = 5;
+            bool expected = true;
+            bool actual;
+            //act
+            customlist.Add(num1);
+            actual = (customlist.capacity = 4);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Add_IntToArray3_AddToArray()
+        {
+            //arrange
+            CustomList<int> customlist = new CustomList<int>();
+            int num1 = 5;
+            bool expected = (count == 1);
+            bool actual;
+            //act
+            customlist.Add(num1);
+            actual = (count == 1);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Add_IntToArray4_AddToArray()
+        {
+            //arrange
+            CustomList<int> customlist = new CustomList<int>();
+            int num1 = 5;
+            bool expected = true;
+            bool actual;
+            //act
+            customlist.Add(num1);
+            actual = (customlist.capacity = 4);
+            //assert
+            Assert.AreEqual(expected, actual);
         }
     }
 }

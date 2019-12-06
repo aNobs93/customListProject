ba25283dff74acf468f0309e8510a5adb9f4d762
Adam Nobs <nobsadam@gmail.com>
2019-12-06 11:30:19 -0600

    Added 7 Remove method unit tests, need to get signed off on them and then it's onto creating the method itself.

--- customListTest/UnitTest1.cs
+++ customListTest/UnitTest1.cs
@@ -130,19 +130,143 @@ namespace customListTest
             //assert
             Assert.AreEqual(expected, actual);
         }
+        
+        [TestMethod]
+        public void Remove_StringFromArray_RemoveFromArray()
+        {
+            //arrange
+            CustomList<string> customlist = new CustomList<string>();
+            string name = "Adam";
+            string otherName = "Allison";
+            string expected = "Allison";
+            string actual;
+            //act
+            customlist.Add(name);
+            customlist.Add(otherName);
+
+            customlist.Remove("Adam");
+            actual = customlist[0];
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Remove_AFewStringsFromArray_RemoveFromArray()
+        {
+            //arrange
+            CustomList<string> customlist = new CustomList<string>();
+            string name = "Adam";
+            string otherName = "Allison";
+            string dogsName = "Hank";
+            string catsName = "Praline";
+            string expected = "Praline";
+            string actual;
+            //act
+            customlist.Add(name);
+            customlist.Add(otherName);
+            customlist.Add(dogsName);
+            customlist.Add(catsName);
+
+            customlist.Remove("Adam");
+            customlist.Remove("Hank");
+            actual = customlist[1];
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
 
         [TestMethod]
-        public void Remove_IntFromEmptyArray_RemoveFromArray()
+        public void Remove_FirstOfSameNumberFromArray_RemoveFromArray()
         {
             //arrange
             CustomList<int> customlist = new CustomList<int>();
             int num = 5;
-            bool expected = false;
-            bool actual;
+            int num1 = 20;
+            int num2 = 10;
+            int num3 = 15;
+            int num4 = 20;
+            int num5 = 25;
+            int expected = 20;
+            int actual;
             //act
-           // actual = customlist.Remove(num);
+            customlist.Add(num);
+            customlist.Add(num1);
+            customlist.Add(num2);
+            customlist.Add(num3);
+            customlist.Add(num4);
+            customlist.Add(num5);
+            customlist.Remove(20);
+            actual = customlist[3]; 
             //assert
-           // Assert.AreEqual(expected, actual);
+            Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void Remove_AllOfSameIntFromArray_RemoveFromArray()
+        {
+            //arrange
+            CustomList<int> customlist = new CustomList<int>();
+            int num = 5;
+            int num1 = 6;
+            int expected = 6;
+            int actual;
+            //act
+            customlist.Add(num);
+            customlist.Add(num);
+            customlist.Add(num);
+            customlist.Add(num);
+            customlist.Add(num);
+            customlist.Add(num1);
+            customlist.Remove(5);
+            customlist.Remove(5);
+            customlist.Remove(5);
+            customlist.Remove(5);
+            customlist.Remove(5);
+            customlist.Remove(6);
+            actual = customlist[0];
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Remove_MakeCountOfArray1_RemoveFromArray()
+        {
+            //arrange
+            CustomList<int> customlist = new CustomList<int>();
+            int num = 5;
+            int num1 = 6;
+            int expected = 1;
+            int actual;
+            //act
+            customlist.Add(num);
+            customlist.Add(num1);
+            customlist.Remove(num);
+            actual = customlist.Count;
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Remove_MakeStringCountOfArray1_RemoveFromArray()
+        {
+            //arrange
+            CustomList<string> customlist = new CustomList<string>();
+            string firstName = "Adam";
+            string lastName = "Nobs";
+            string herFirstName = "Allison";
+            string herLastName = "Raison";
+            int expected = 1;
+            int actual;
+            //act
+            customlist.Add(firstName);
+            customlist.Add(lastName);
+            customlist.Add(herFirstName);
+            customlist.Add(herLastName);
+            customlist.Remove(firstName);
+            customlist.Remove(lastName);
+            customlist.Remove(herFirstName);
+            actual = customlist.Count;
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+        /////////////////////////////////////////////////////////
     }
 }
